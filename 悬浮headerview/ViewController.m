819053bfcbab417dcdef7293e1119f3993e989c9
zeng yong <zengyongeryu@163.com>
2016-06-06 08:57:49 +0800

    Initial Commit

---
+++ 悬浮headerview/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  悬浮headerview
+//
+//  Created by 曾勇 on 16/6/6.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 悬浮headerview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  悬浮headerview
+//
+//  Created by 曾勇 on 16/6/6.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
